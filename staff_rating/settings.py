7f00568b0a310e9d6f15168874854cde72bd062b
Mohammad Asim Khan <ak4514768@gmail.com>
2021-07-24 13:30:56 +0530

    password reset added

--- staff_rating/settings.py
+++ staff_rating/settings.py
@@ -130,3 +130,4 @@ AUTH_USER_MODEL = 'users.MyUser' #new
 LOGIN_REDIRECT_URL = 'home' #new
 LOGOUT_REDIRECT_URL = 'home' #new
 CRISPY_TEMPLATE_PACK = 'bootstrap4' #new
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #new
